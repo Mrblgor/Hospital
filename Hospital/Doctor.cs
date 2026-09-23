15e0c6f37bfb6b0320e8da5421a982b1530c860e
Mrblgor <igor.bolotov.19@bk.ru>
2026-09-23 20:12:37 +0700

    Update Doctor.cs

--- Hospital/Doctor.cs
+++ Hospital/Doctor.cs
@@ -1,34 +1,47 @@
-﻿namespace Hospital;
+namespace Hospital;
 
 /// <summary>
 /// Представляет врача больницы.
 /// </summary>
 internal class Doctor
 {
+    private int _id;
+
     /// <summary>
-    /// Уникальный идентификатор врача.
+    /// Уникальный идентификатор врача (только для чтения).
     /// </summary>
-    public int Id { get; set; }
+    public int Id => _id;
 
     /// <summary>
     /// ФИО врача.
     /// </summary>
-    public string FullName { get; set; }
+    public string FullName { get; init; }
 
     /// <summary>
     /// Идентификатор отделения, в котором работает врач.
     /// </summary>
-    public int DepartmentId { get; set; }
+    public int DepartmentId { get; init; }
 
     /// <summary>
     /// Специальность врача.
     /// </summary>
-    public string Specialty { get; set; }
+    public string Specialty { get; init; }
 
     /// <summary>
     /// Признак того, что врач является хирургом.
     /// </summary>
-    public bool IsSuregion => Specialty == "Хирург";
+    public bool IsSurgeon => Specialty == "Хирург";
+
+    /// <summary>
+    /// Конструктор с полным набором параметров.
+    /// </summary>
+    public Doctor(int id, string fullName, int departmentId, string specialty)
+    {
+        _id = id;
+        FullName = fullName;
+        DepartmentId = departmentId;
+        Specialty = specialty;
+    }
 
     /// <summary>
     /// Возвращает строковое представление информации о враче.
