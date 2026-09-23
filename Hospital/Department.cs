763e85b20a47f4f2433d5623332528b8ddc4b83b
Mrblgor <igor.bolotov.19@bk.ru>
2026-09-23 20:12:23 +0700

    Update Department.cs

--- Hospital/Department.cs
+++ Hospital/Department.cs
@@ -1,24 +1,36 @@
-﻿namespace Hospital;
+namespace Hospital;
 
 /// <summary>
 /// Представляет отделение больницы.
 /// </summary>
 internal class Department
 {
+    private int _id;
+
     /// <summary>
-    /// Уникальный идентификатор отделения.
+    /// Уникальный идентификатор отделения (только для чтения).
     /// </summary>
-    public int Id { get; set; }
+    public int Id => _id;
 
     /// <summary>
     /// Название отделения.
     /// </summary>
-    public string Name { get; set; }
+    public string Name { get; init; }
 
     /// <summary>
     /// ФИО заведующего отделением.
     /// </summary>
-    public string Head { get; set; }
+    public string Head { get; init; }
+
+    /// <summary>
+    /// Конструктор с полным набором параметров.
+    /// </summary>
+    public Department(int id, string name, string head)
+    {
+        _id = id;
+        Name = name;
+        Head = head;
+    }
 
     /// <summary>
     /// Возвращает строковое представление информации об отделении.
